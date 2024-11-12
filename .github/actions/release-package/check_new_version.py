06a77d2231c415b158700a6f8ad565ec91a1fa84
S1lv3r21 <banevarez03@gmail.com>
2024-11-12 12:02:09 -0800

    Update check_new_version.py

--- .github/actions/release-package/check_new_version.py
+++ .github/actions/release-package/check_new_version.py
@@ -13,15 +13,17 @@ with open("RELEASE_NOTES.md", "r") as file:
         if line.strip().startswith('v'):
             VERSION = line.strip().replace("v", "")
             print(f"version: {VERSION}")
-            cmd = f"git rev-parse --verify refs/tags/v{VERSION}"  # check if tag already exists
+            cmd = f"git rev-parse --verify v{VERSION} 2>/dev/null"  # check if tag already exists
             print(f"cmd: {cmd}")
             result = subprocess.run(cmd, shell=True, capture_output=True)
-            RESULT = result.returncode # return 128 if exist otherwise return 0 
+            RESULT = result.returncode # return 128 if exist otherwise return 0
             break
 
 if not VERSION:
     print("Version not found in RELEASE_NOTES.md file.")
     sys.exit(1)
+print(f"RESULT={RESULT}")
+print(f"VERSION={VERSION}")
 # set environment variable for GitHub Action usage
 with open(os.getenv('GITHUB_ENV'), 'a') as git_env:
     git_env.write(f"RESULT={RESULT}\n")
