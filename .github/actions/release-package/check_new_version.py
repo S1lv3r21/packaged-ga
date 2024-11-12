c0e83c7c0a50a0fb20274164e79ad5b18143f0c7
S1lv3r21 <banevarez03@gmail.com>
2024-11-12 11:39:02 -0800

    Update check_new_version.py

--- .github/actions/release-package/check_new_version.py
+++ .github/actions/release-package/check_new_version.py
@@ -1,23 +1,45 @@
 """Retrieve the latest version in RELEASE_NOTES.md and report if the version is a new release or not."""
-import subprocess
 import os
+import re
+import sys
+import subprocess
+
 
 # default values for empty release_notes.md file
 RESULT = "-1" 
 VERSION = ""   
+PKG = ""
 
-with open('RELEASE_NOTES.md', 'r') as file:
+with open("RELEASE_NOTES.md", "r") as file:
     for line in file:
         if line.strip().startswith('v'):
             VERSION = line.strip().replace("v", "")
             print(f"version: {VERSION}")
-            cmd = f"git rev-parse v{VERSION}"  # check if tag already exists
+            cmd = f"git rev-parse --verify refs/tags/v{VERSION}"  # check if tag already exists
             print(f"cmd: {cmd}")
             result = subprocess.run(cmd, shell=True, capture_output=True)
             RESULT = result.returncode # return 128 if exist otherwise return 0 
             break
 
+with open("pyproject.toml", "r") as file:
+    for line in file:
+        l = line
+        if l.strip().startswith("name"):
+            pattern = r'"([^"]*)"'
+            # Find the content inside the quotes
+            match = re.search(pattern, l)
+            if match:
+                PKG = match.group(1).strip()
+            break
+if not PKG:
+    print("Package name not found in pyproject.toml file.")
+    sys.exit(1)
+
+if not VERSION:
+    print("Version not found in RELEASE_NOTES.md file.")
+    sys.exit(1)
 # set environment variable for GitHub Action usage
 with open(os.getenv('GITHUB_ENV'), 'w+') as git_env:
+    git_env.write(f"PKG={PKG}\n")
     git_env.write(f"RESULT={RESULT}\n")
     git_env.write(f"VERSION={VERSION}")
